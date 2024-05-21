ecfcc49ad306e15fec5c1377395a3f692e80f94c
pupil <pupil@ivkhk.ee>
2024-05-21 14:16:58 +0300

    db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('photo_world_jktv22', 'root', '',{
-    host: 'localhost',
+const db = new Sequelize('g1mgv0h0ixsjvdmw', 'p3owvn0flp97kwvo', 'fdgelcr161qiped4', {
+    host: 'q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
 });
 
